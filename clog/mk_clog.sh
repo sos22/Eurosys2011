d00cbbff18457efc9cc959cf56707d42ca63aeed
Steven Smith <sos22@cl.cam.ac.uk>
2010-10-10 14:06:31 +0100

    We now have the data from the repeated runs of the clog test.

--- clog/mk_clog.sh
+++ clog/mk_clog.sh
@@ -9,12 +9,13 @@ mkdir output
 
 depths="1 2 5 10 20 50"
 
-for repeat in 1
+for repeat in 1 2 3
 do
     for depth in $depths
     do
-	grep "By class" data${repeat}/timing.${depth}.txt | sed 's/.*building CM \([0-9.]*\), gen fixes \([0-9.]*\)/\1 \2/' | (
-	    read build_machine gen_fix
+	grep "By class" data${repeat}/timing.${depth}.txt | sed 's/ *\([0-9.]*\).*building CM \([0-9.]*\), gen fixes \([0-9.]*\)/\1 \2 \3/' | (
+	    read inc_replay build_machine gen_fix
+	    echo $inc_replay >> output/inc_replay.$depth
 	    echo $build_machine >> output/build_cms.$depth
 	    echo $gen_fix >> output/gen_fixes.$depth
 	    python -c "print $build_machine + $gen_fix" >> output/totals.$depth )
@@ -23,7 +24,7 @@ done
 
 for depth in $depths
 do
-    for field in build_cms gen_fixes totals
+    for field in inc_replay build_cms gen_fixes totals
     do
 	mean=$(summarise_numbers.py < output/${field}.${depth} | grep mean | cut -d' ' -f 2)
 	sd=$(summarise_numbers.py < output/${field}.${depth} | grep sd | cut -d' ' -f 2)
